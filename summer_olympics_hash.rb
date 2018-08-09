57dae388f92c2f373a389208d469a673d44b391e
PythonKittyGirl <ung.deborah@gmail.com>
2018-08-09 18:13:34 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,7 +1,7 @@
 
 def create_olympics_hash
   # Implement this method so that it returns a hash with the data provided on README.md
-  olympic_hash = {
+  olympics_hash = {
     :Sydney => "2000"
     :Athens => "2008"
     :Bejing => "2008"
