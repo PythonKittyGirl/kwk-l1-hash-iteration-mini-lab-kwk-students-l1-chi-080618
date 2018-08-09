85b5510767e16ff0be655ff7a1ad1eb21264dfaa
PythonKittyGirl <ung.deborah@gmail.com>
2018-08-09 18:35:37 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -31,7 +31,7 @@ end
 def iterate_through_keys
   # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
   # into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
-  
+  iterate = add_a_key_value_pair
   new_thing = iterate
   new_thing.upcase do |place, year|
     return new_thing
